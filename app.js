a6ce02c0dd7b058dba08abae24569a1c8054c86c
marcovittoriozacca <zaccamarco01@gmail.com>
2024-05-24 14:51:16 +0200

    feat: posts controller with index function to display all posts inside the json file

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
-
 const app = express();
+
+const {index} = require('./controllers/post.js');
 
 const port = process.env.PORT || 3000;
 const host = process.env.HOST || "localhost";
@@ -9,7 +10,7 @@ app.get('/', (req,res) =>{
     res.send('<h1>Benvenuto nel mio blog!</h1>');
 })
 
-app.get('/posts');
+app.get('/posts', index);
 
 app.listen(port, host, () => {
     console.log(`http://${host}:${port}`);
--- controllers/post.js
+++ controllers/post.js
@@ -1,5 +1,34 @@
+const {getFile} = require('../utilities.js');
+
+const index = (req, res) => {
+    res.format({
+        "html": () => {
+
+            const postsFile = getFile('posts', 'json');
+
+            let html = `<ul>`;
+            postsFile.forEach(({title, description, tags, img}) => {
+                html += `<li>
+                            <h2>${title}</h2>
+                            <img src="/${img}" alt="${title}">
+                            <p>${description}</p
+                        `;
+                tags.forEach(tag => {
+                    html+= `
+                        <span>${tag}</span> <br>
+                    `
+                })
+                html += `</li>`
+            });
+            html += `</ul>`
+            res.type("html");
+            res.send(html);
+            
+        }
+    })
+}
 
 
 module.exports = {
-
+    index,
 }
