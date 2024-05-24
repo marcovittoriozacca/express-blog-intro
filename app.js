ca9b216d51654bb3c573b6e5053e1310db88e42a
marcovittoriozacca <zaccamarco01@gmail.com>
2024-05-24 15:18:56 +0200

    feat: html / json based on the header: Application

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const app = express();
 
-const {index} = require('./controllers/post.js');
+const post = require('./controllers/post.js');
 
 const port = process.env.PORT || 3000;
 const host = process.env.HOST || "localhost";
 
+//main route with a simple h1 inside as a response
 app.get('/', (req,res) =>{
     res.send('<h1>Benvenuto nel mio blog!</h1>');
 })
 
-app.get('/posts', index);
+//posts route with index as a controller from the posts controller
+app.get('/posts', post.index);
 
 app.listen(port, host, () => {
     console.log(`http://${host}:${port}`);
--- controllers/post.js
+++ controllers/post.js
@@ -1,34 +1,37 @@
 const {getFile} = require('../utilities.js');
 
-const index = (req, res) => {
-    res.format({
-        "html": () => {
+const htmlPage = (fileName, ext) => {
+    const postsFile = getFile(fileName, ext);
 
-            const postsFile = getFile('posts', 'json');
+    let html = `<ul>`;
+    postsFile.forEach(({title, description, tags, img}) => {
+        html += `<li>
+                    <h2>${title}</h2>
+                    <img src="/${img}" alt="${title}">
+                    <p>${description}</p>
+                `;
+        tags.forEach(tag => {
+            html+= `<span>${tag}</span><br>`
+        })
+        html += `</li>`
+    });
+    html += `</ul>`
+    
+    return html;
+}
 
-            let html = `<ul>`;
-            postsFile.forEach(({title, description, tags, img}) => {
-                html += `<li>
-                            <h2>${title}</h2>
-                            <img src="/${img}" alt="${title}">
-                            <p>${description}</p
-                        `;
-                tags.forEach(tag => {
-                    html+= `
-                        <span>${tag}</span> <br>
-                    `
-                })
-                html += `</li>`
-            });
-            html += `</ul>`
-            res.type("html");
-            res.send(html);
-            
-        }
-    })
+const jsonFile = (fileName, ext) => {
+    const postsFile = getFile(fileName, ext);
+    return postsFile;
 }
 
 
+const index = (req, res) => {
+    res.format({
+        "html": () => res.type("html").send(htmlPage('posts', 'json')),
+        "json": () => res.type("json").send(jsonFile('posts', 'json'))
+    })
+}
 module.exports = {
     index,
 }
